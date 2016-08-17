39c38970ff7fdc374a4a9893df2b7c9313a93604
spetser <thomas.capiau@gmail.com>
2016-08-17 14:23:29 +0200

    Add files via upload

---
+++ wsite/scripts/main.js
@@ -0,0 +1,9 @@
+﻿var myImage = document.querySelector('img');
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/sunrise.jpg') {
+      myImage.setAttribute ('src','images/serious.jpg');
+    } else {
+      myImage.setAttribute ('src','images/sunrise.jpg');
+    }
+}
