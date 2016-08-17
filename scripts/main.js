182d46a622fe89756193005d19b4bba098aced18
spetser <thomas.capiau@gmail.com>
2016-08-17 14:22:57 +0200

    Delete main.js

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-﻿var myImage = document.querySelector('img');
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/sunrise.jpg') {
-      myImage.setAttribute ('src','images/serious.jpg');
-    } else {
-      myImage.setAttribute ('src','images/sunrise.jpg');
-    }
-}
